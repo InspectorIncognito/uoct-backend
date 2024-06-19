935b17a0c4a49a531a69d2c39e31d2920f356caf
Mrtn-fa <mnfaray@gmail.com>
2024-06-18 21:36:05 -0400

    fix(env): Fixed env key error
    Fixed PROTO_URL key error

--- backend/gtfs_rt/config.py
+++ backend/gtfs_rt/config.py
@@ -1,7 +1,11 @@
-from pathlib import Path
+import decouple
 from decouple import config
+from pathlib import Path
 from django.utils.timezone import get_current_timezone
 
 ROOT_PATH = Path(__file__).parent
-PROTO_URL = config('PROTO_URL')
+try:
+    PROTO_URL = config('PROTO_URL')
+except decouple.UndefinedValueError:
+    PROTO_URL = None
 TIMEZONE = get_current_timezone()
