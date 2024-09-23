eac75ab9551a45a9196367cb24d358c9823fd866
Mrtn-fa <mnfaray@gmail.com>
2024-09-23 17:56:16 -0300

    fix(HistoricSpeed): fixed historic speed value
    Fixed historic speed calc algorithm
    Fixed factories & tests

--- backend/gtfs_rt/management/commands/get_last_month_avg_speed.py
+++ backend/gtfs_rt/management/commands/get_last_month_avg_speed.py
@@ -1,8 +1,12 @@
 from django.core.management.base import BaseCommand
-from processors.speed.avg_speed import get_last_month_avg_speed
+from processors.speed.avg_speed import get_month_commercial_speed
+from django.utils import timezone
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Calling get_last_month_avg_speed command...")
-        get_last_month_avg_speed()
+        today = timezone.localtime()
+        this_year = today.year
+        this_month = today.month
+        get_month_commercial_speed(year=this_year, month=this_month)
--- backend/processors/speed/avg_speed.py
+++ backend/processors/speed/avg_speed.py
@@ -1,64 +1,40 @@
 from rest_api.models import Speed, HistoricSpeed, Segment
 from datetime import datetime, timedelta
-from django.db.models import Avg, Sum
 from django.utils import timezone
-from django.db.models import Func
-
-
-class Round2(Func):
-    function = "ROUND"
-    template = "%(function)s(%(expressions)s::numeric, 2)"
+from django.db.models import Sum, ExpressionWrapper, FloatField
+from django.db.models.functions import Round
 
 
 def flush_historic_speeds():
     HistoricSpeed.objects.all().delete()
 
 
-def get_last_month_avg_speed():
-    print("Calling get_last_month_avg_speed command...")
-    now = timezone.localtime()
-    yesterday = now - timedelta(days=1)
-    last_month = yesterday.month
-    last_year = yesterday.year
-
-    last_month_speeds = Speed.objects.filter(timestamp__year=last_year, timestamp__month=last_month)
-    avg_speeds = last_month_speeds.values("segment", "day_type", "temporal_segment", "timestamp").annotate(
-        average_segment_speed=Round2(Avg("speed"))).order_by("segment", "day_type", "temporal_segment")
-    for historic_speed in avg_speeds:
-        historic_speed_data = {
-            "segment": Segment.objects.get(pk=historic_speed["segment"]),
-            "speed": historic_speed["average_segment_speed"],
-            "day_type": historic_speed["day_type"],
-            "temporal_segment": historic_speed["temporal_segment"],
-            "timestamp": yesterday
-        }
-        HistoricSpeed.objects.create(**historic_speed_data)
-
-
-def get_avg_speed_by_month(year, month):
-    start_date = datetime(year, month, 1, 0, 0, 0, 0)
-    if month == 12:
-        end_date = datetime(year + 1, 1, 1, 0, 0, 0)
-    else:
-        end_date = datetime(year, month + 1, 1, 0, 0, 0)
-
-    start_date = timezone.make_aware(start_date, timezone.get_current_timezone())
-    end_date = timezone.make_aware(end_date, timezone.get_current_timezone())
-
-    speeds_in_month = Speed.objects.filter(
-        timestamp__gte=start_date,
-        timestamp__lt=end_date,
+def get_month_commercial_speed(year: int, month: int):
+    print("Calling get_month_commercial_speed command...")
+    creation_date = timezone.localtime().replace(year=year, month=month, day=1, hour=0, minute=0, second=0,
+                                                 microsecond=0)
+    last_month_speeds = Speed.objects.filter(timestamp__year=year, timestamp__month=month)
+    avg_speeds = (
+        last_month_speeds
+        .values('segment', 'day_type', 'temporal_segment')
+        .annotate(
+            total_distance=Sum('distance'),
+            total_time=Sum('time_secs'),
+            avg_speed=ExpressionWrapper(
+                Round(3.6 * Sum('distance') / Sum('time_secs'), 2),
+                output_field=FloatField()
+            )
+        )
     )
-
-    avg_speeds = speeds_in_month.values('segment', 'temporal_segment', 'day_type').annotate(
-        avg_speed=Round2(Sum('distance')/Sum('time_secs') * 3.6)
-    ).order_by('segment', 'temporal_segment', 'day_type')
-    #avg_speeds = (speeds_in_month.
-    #              values("segment", "day_type")
-    #              .annotate(avg_speed=Avg("speed"))
-    #              .order_by("segment", "day_type")
-    #              )
-    return avg_speeds
+    for avg_speed in avg_speeds:
+        historic_speed_data = dict(
+            segment=Segment.objects.get(pk=avg_speed['segment']),
+            speed=avg_speed['avg_speed'],
+            day_type=avg_speed['day_type'],
+            temporal_segment=avg_speed['temporal_segment'],
+            timestamp=creation_date
+        )
+        HistoricSpeed.objects.create(**historic_speed_data)
 
 
 def save_historic_avg_speed(avg_speeds, custom_datetime=None):
@@ -67,4 +43,11 @@ def save_historic_avg_speed(avg_speeds, custom_datetime=None):
                                "day_type": avg_speed["day_type"],
                                "speed": avg_speed["avg_speed"],
                                'timestamp': custom_datetime if custom_datetime is not None else None}
-        HistoricSpeed.objects.create(**historic_speed_data)
+
+
+def get_last_month_avg_speed():
+    print("Calling get_last_month_avg_speed command...")
+    today = timezone.localtime() - timedelta(days=1)
+    this_year = today.year
+    this_month = today.month
+    get_month_commercial_speed(year=this_year, month=this_month)
