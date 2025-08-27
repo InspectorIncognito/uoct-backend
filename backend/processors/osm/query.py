203204e9b59523355ac142a7de3dc30b6ee70adf
Santiago Haberle <santiago.haberle@ug.uchile.cl>
2025-08-27 13:11:20 -0400

    refactor(query): reorganize imports and enhance function documentation

--- backend/processors/osm/query.py
+++ backend/processors/osm/query.py
@@ -1,6 +1,5 @@
-import overpass
 import geojson
-import geopandas as gpd
+import overpass
 
 ALAMEDA_QUERY = """
     rel(1674530);
@@ -16,6 +15,19 @@ ALAMEDA_QUERY = """
 
 
 def overpass_query(query):
+    """Executes an Overpass API query and returns the results.
+    To update this function to download more data in an structured way, look for the code in the following repo https://github.com/shaberle/traffic-incident-detection/blob/main/codigo/osm.py
+
+    Parameters
+    ----------
+    query : str
+        The Overpass API query to execute.
+
+    Returns
+    -------
+    dict
+        The GeoJSON response from the Overpass API.
+    """
     api = overpass.API()
-    response = api.get(query, verbosity='geom')
+    response = api.get(query, verbosity="geom")
     return geojson.loads(geojson.dumps(response))
