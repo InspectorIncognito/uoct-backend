8a9a09fce3ad6094fa73fc1617f50cfa30ddf3fd
Santiago Haberle <santiago.haberle@ug.uchile.cl>
2025-09-26 09:19:55 -0300

    fix(command): correct command name from process_shape_data to process_osm_queries in initialize_map_data

--- backend/rest_api/management/commands/initialize_map_data.py
+++ backend/rest_api/management/commands/initialize_map_data.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
         # Create Shapes and Segments
         if not options["use_fixture"]:
             # Problema con la función split_geojson_by_shape en osm/process.py. No esta captando bien las direcciones.
-            call_command("process_shape_data")
+            call_command("process_osm_queries")
         else:
             call_command("process_fixture_data")
         # Create GTFSShapes
