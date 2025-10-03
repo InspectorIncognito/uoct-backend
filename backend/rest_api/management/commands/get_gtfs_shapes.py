434c7e34e424d4ed0ff554b2f570f67713351ac4
Santiago Haberle <santiago.haberle@ug.uchile.cl>
2025-10-03 11:25:57 -0300

    feat(migrations): add Axles model with name, streets, and city fields

--- backend/rest_api/management/commands/get_gtfs_shapes.py
+++ backend/rest_api/management/commands/get_gtfs_shapes.py
@@ -1,17 +1,20 @@
+import geopandas as gpd
 from django.core.management import BaseCommand, CommandError
-from rest_api.models import Shape, Segment, GTFSShape
-from shapely.geometry import LineString as shp_LineString
+from rest_api.models import GTFSShape, Segment, Shape
 from rest_api.util.gtfs import GTFSShapeManager
 from shapely import to_geojson
-import geopandas as gpd
+from shapely.geometry import LineString as shp_LineString
 from velocity.gtfs import GTFSManager
 
 
 class Command(BaseCommand):
-    help = 'Read the GTFS file to get all the shapes and their direction'
+    help = "Read the GTFS file to get all the shapes and their direction"
 
     def handle(self, *args, **options):
         print("Calling get_gtfs_shapes command...")
         gtfs_manager = GTFSManager()
         processed_shapes = gtfs_manager.get_processed_df()
+        # Save df to a csv file for debugging
+        processed_shapes.to_csv("processed_shapes.csv", index=False)
+        # Save to DB
         gtfs_manager.save_gtfs_shapes_to_db(processed_shapes)
