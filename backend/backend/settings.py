8bbba4cc545ce8d28870519c308431210e846fb0
Santiago Haberle <santiago.haberle@ug.uchile.cl>
2025-12-04 08:51:23 -0300

    refactor: remove CSRF exemption middleware for API endpoints

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "backend.middleware.CSRFExemptAPIMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
